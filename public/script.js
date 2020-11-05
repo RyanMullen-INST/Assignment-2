930507b021dbe782969f08252a0f0c8514b90d8f
RyanMullenINST <44683492+RyanMullenCMSC@users.noreply.github.com>
2020-11-05 04:43:33 -0500

    Added some stuff from lecture

--- public/script.js
+++ public/script.js
@@ -1,2 +1,21 @@
 // This is where we do script stuff
 // WEEEEEEEE
+// I pulled this straight from Lab 7
+// We will likely need to add extra stuff because this is only the event listener object
+// For consistency's sake, please add it ABOVE the Event Listener
+document.body.addEventListener('submit', async (e) => {
+    e.preventDefault(); // this stops whatever the browser wanted to do itself.
+    const form = $(e.target).serializeArray();
+    fetch('/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(form)
+    })
+      .then((fromServer) => fromServer.json())
+      .then((jsonFromServer) => runThisWithResultsFromServer(jsonFromServer))
+      .catch((err) => {
+        console.log(err);
+      });
+  });
